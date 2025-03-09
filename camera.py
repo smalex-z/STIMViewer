mirroring and scaling fixed

camera.py
@@ -396,7 +396,7 @@ class Camera:
                 self.translation_matrix = homography_matrix
                 print("✅ Homography Computed Successfully!")
 
-                self._interface.on_projection_received(np.array(cv2.imread("./Assets/CalibOutput.jpg")))
+                self._interface.on_projection_received(np.array(cv2.imread("./Assets/custom_registration_image.png")), self.translation_matrix)
             except Exception as e:
                 print(f"❌ Error calculating homography: {e}")
 

projection.py
@@ -32,8 +32,10 @@ class ProjectDisplay(QMainWindow):
         if homography_matrix is None:
             homography_matrix = np.eye(3)
 
+        image_transformed = cv2.flip(image, 1)
+
         # Apply Homography Transformation
-        image_transformed = cv2.warpPerspective(image, homography_matrix, (image.shape[1], image.shape[0]))
+        image_transformed = cv2.warpPerspective(image_transformed, homography_matrix, (image.shape[1], image.shape[0]))
         image_transformed = cv2.cvtColor(image_transformed, cv2.COLOR_BGR2RGB)  # Convert to RGB
 
         # ✅ Directly update the image (since we're in the GUI thread)